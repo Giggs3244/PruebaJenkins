bd81b92a40b5f32551b5953584ff0b8fd7904ff2
Giggs3244 <bryan.bedoy@gmail.com>
2016-09-10 15:41:25 -0500

    First commit

---
+++ src/clases/Principal.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *
+ * @author giggs_000
+ */
+public class Principal extends JFrame {
+
+    public static void main(String a[]) {
+        Casa casa = new Casa();
+    }
+
+}
+
+class Casa extends JFrame {
+    
+    public Casa(){        
+        setSize(400, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        triangle = new Polygon(new int[]{50, 200, 350}, new int[]{200, 50, 200}, 3);
+        square = new Polygon(new int[]{50, 50, 350, 350}, new int[]{400, 200, 200, 400}, 4);
+    }
+    
+    private Polygon triangle;
+    private Polygon square;
+    
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(Color.red);
+        g.fillPolygon(triangle);
+        g.setColor(Color.lightGray);
+        g.fillPolygon(square);
+    }
+}
